1c06cc26c4615c4cbe848ea3fa07eb256afa0f91
Shen Zhifeng <shen_zhifeng@outlook.com>
2020-01-18 01:09:17 +0800

    change output

--- model/src/main/java/fun/xuefeng/App.java
+++ model/src/main/java/fun/xuefeng/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         //Add some comments
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! Hello World!" );
     }
 }
